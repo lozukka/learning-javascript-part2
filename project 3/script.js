8a90f743bf9ec6cca6d4ad8bd5fdf61a3172711d
lozukka <lotsulotsu@gmail.com>
2025-09-05 14:51:56 +0300

    worked on main logic

--- project 3/script.js
+++ project 3/script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(question =>{
+    question.addEventListener("click", ()=>{
+        const answer = question.nextElementSibling;
+        // suljetaan kaikki muut vastaukset
+    document.querySelectorAll(".hidden").forEach((a) => {
+      if (a !== answer) {
+        a.classList.remove("show");
+      }
+    });
+        answer.classList.toggle("show");
+    })
+})
+
