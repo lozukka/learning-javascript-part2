12861c42ff8575f37f4ca675c251cc467f789ad7
lozukka <lotsulotsu@gmail.com>
2025-09-05 14:55:00 +0300

    Update script.js

--- project 3/script.js
+++ project 3/script.js
@@ -3,7 +3,7 @@ const questions = document.querySelectorAll(".question")
 questions.forEach(question =>{
     question.addEventListener("click", ()=>{
         const answer = question.nextElementSibling;
-        // suljetaan kaikki muut vastaukset
+        
     document.querySelectorAll(".hidden").forEach((a) => {
       if (a !== answer) {
         a.classList.remove("show");
