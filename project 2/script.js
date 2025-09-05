7772b737008331b38aba1dc8a3d9afad11833b89
lozukka <lotsulotsu@gmail.com>
2025-09-05 10:54:06 +0300

    Update script.js
    variables and making sure they are connected to html

--- project 2/script.js
+++ project 2/script.js
@@ -1,1 +1,14 @@
-console.log("helloo")
+let rollBtn = document.getElementById("roll");
+let dice1 = document.getElementById("dice1");
+let dice2 = document.getElementById("dice2");
+let totalResult = document.getElementById("result");
+let resetBtn = document.getElementById("reset");
+let history = document.getElementById("history");
+let historyList = [];
+
+rollBtn.addEventListener("click", (event)=>{
+dice1.textContent="2";
+dice2.textContent="3";
+totalResult.textContent="Hurray";
+history.textContent="very first roll";
+})
