c2c6431c4818c0c05764721edbfad15525f916fe
lozukka <lotsulotsu@gmail.com>
2025-09-08 12:43:21 +0300

    state machine model

--- project 5/script.js
+++ project 5/script.js
@@ -1,1 +1,29 @@
-console.log("hello")
+let light = document.getElementsByClassName("light")[0];
+let interval;
+let currentState = "green";
+const transitions = {
+    green: "yellow",
+    yellow: "red",
+    red: "green"
+};
+startTimer();
+render();
+
+function startTimer(){
+    clearInterval(interval);
+    interval = setInterval(changeState, 5000);
+};
+
+function changeState(){
+    currentState = transitions[currentState];
+    render();
+}
+
+function render(){
+    light.classList.remove("green", "yellow", "red");
+    light.classList.add(currentState);
+    console.log("current state: ", currentState);
+}
+function stopTimer(){
+    clearInterval(interval);
+}
