2815001b12fc9f55fc79d4bd56ed6bbc64b294b2
lozukka <lotsulotsu@gmail.com>
2025-09-08 10:36:05 +0300

    main logic
    worked on main logic

--- project 4/script.js
+++ project 4/script.js
@@ -1,1 +1,28 @@
-console.log(hello)
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+    let slideNumber = document.getElementById("numbertext");
+let slideIndex = 1;
+showSlide();
+updateDots();
+
+function plusSlides(n){
+    slideIndex += n;
+    if(slideIndex > slides.length){slideIndex = 1}
+    if(slideIndex < 1){slideIndex = slides.length}
+    showSlide();
+    updateDots();
+}
+
+function showSlide(){
+    for(let i = 0; i<slides.length; i++){
+        slides[i].style.display ="none";
+    }
+    slides[slideIndex -1].style.display = "block";
+    slideNumber.textContent = slideIndex;
+}
+function updateDots(){
+    for(let i = 0; i<dots.length; i++){
+        dots[i].classList.remove("active");
+    }
+    dots[slideIndex -1].classList.add("active");
+}
