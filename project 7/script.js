fb9eeb6178780f01c6e5390a6b623fb127365ad9
lozukka <lotsulotsu@gmail.com>
2025-09-09 09:19:22 +0300

    local storage

--- project 7/script.js
+++ project 7/script.js
@@ -1,5 +1,10 @@
 console.log("hello")
 // variables
+let notes = [];
+let notesDisplay = document.getElementById("saved-notes");
+let saveBtn = document.getElementById("save-note");
+let deleteBtn = document.getElementById("delete-note");
+let noteText = document.getElementById("note-text");
 
 //----------------
 //main function
@@ -7,6 +12,13 @@ console.log("hello")
 //eventlistener->fetch userinput->validate note
 //save note local
 //render note
+saveBtn.addEventListener("click", (event)=>{
+    let text = noteText.value;
+    notes.push(text);
+    saveNotes();
+    renderNotesDisplay(text);
+    noteText.value = "";
+})
 
 //-----------------
 //helper functions
@@ -14,11 +26,26 @@ console.log("hello")
 //fetch user input
 //validate note
 //save note to local
-//render note
-
+function saveNotes() {
+        localStorage.setItem("myNotes", JSON.stringify(notes)); 
+      }
+//render notes
+function renderNotesDisplay(text) {
+        let li = document.createElement("li");
+        li.textContent = text;
+        notesDisplay.appendChild(li);
+      }
 //edit the note
 
 //delete the note
 
 //render notes when refreash page
+window.addEventListener("load", loadNotes);
+function loadNotes() {
+        const saved = localStorage.getItem("myNotes");
+        if (saved) {
+          notes = JSON.parse(saved); 
+          notes.forEach((item) => renderNotesDisplay(item)); 
+        }
+      }
 
