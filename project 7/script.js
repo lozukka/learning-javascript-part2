24fb5f353ff47d86baaefa8af73657fa3ea998fc
lozukka <lotsulotsu@gmail.com>
2025-09-09 08:55:06 +0300

    Update script.js

--- project 7/script.js
+++ project 7/script.js
@@ -1,1 +1,24 @@
 console.log("hello")
+// variables
+
+//----------------
+//main function
+//----------------
+//eventlistener->fetch userinput->validate note
+//save note local
+//render note
+
+//-----------------
+//helper functions
+//-----------------
+//fetch user input
+//validate note
+//save note to local
+//render note
+
+//edit the note
+
+//delete the note
+
+//render notes when refreash page
+
