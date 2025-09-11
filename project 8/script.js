651c8a72eb7d78fc2c269af0b97db38b75c75afe
lozukka <lotsulotsu@gmail.com>
2025-09-11 09:56:54 +0300

    random index number

--- project 8/script.js
+++ project 8/script.js
@@ -1,4 +1,14 @@
 console.log("hello")
+
+function getQuote(){
+    let index = Math.floor(Math.random()*20)+1;
+    console.log(index);
+    let quote = fetchQuote(index);
+    
+}
+
+function fetchQuote(index){
 fetch('./quotes.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
+}
