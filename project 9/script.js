01fca1f71f3b19520b4b8a7599b855f14664cfe0
lozukka <lotsulotsu@gmail.com>
2025-09-11 11:57:20 +0300

    Update script.js

--- project 9/script.js
+++ project 9/script.js
@@ -1,1 +1,30 @@
-console.log("hello")
+async function getWeather(){
+    const {temperature, windSpeed, condition} = await fetchWeather();
+    renderWeather(temperature, windSpeed, condition);
+}
+
+async function fetchWeather(){
+    try {
+        const response = await fetch("./weather.json");
+        if(!response.ok) throw new Error ("Failed to load weather");
+
+        const data = await response.json();
+        const index = Math.floor(Math.random()* data.weatherReports.length);
+        return data.weatherReports[index];
+
+    } catch (error) {
+        console.log(error);
+        return {temperature: "Failed to load weather", windSpeed: "", condition: ""};
+    }
+    
+}
+
+function renderWeather(temperature, windSpeed, condition){
+    let temperatureText = document.getElementById("temperature");
+    let windText = document.getElementById("wind");
+    let conditionText = document.getElementById("condition");
+
+    temperatureText.textContent = temperature;
+    windText.textContent = windSpeed;
+    conditionText.textContent = condition;
+}
